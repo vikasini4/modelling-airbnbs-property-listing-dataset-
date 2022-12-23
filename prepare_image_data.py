277c69569d5b1afc65bb3478fcae1018a76ba409
vikasini4 <96987281+vikasini4@users.noreply.github.com>
2022-12-23 23:08:59 +0000

    Update prepare_image_data.py

--- prepare_image_data.py
+++ prepare_image_data.py
@@ -4,35 +4,6 @@ from PIL import Image
 import pandas as pd
 
 
-#def download_images():
-# Set up the S3 client
-#client = boto3.client('s3',
-                  #aws_access_key_id="AKIA25P26T62ZQL6HPPM",
-                  #aws_secret_access_key="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")
-
-#bucket = 'code-airbnb-property-listings'
-#cur_path = os.getcwd()
-#file = 'Screenshot 2022-12-22 at 19.16.35.png'
-#filename = os.path.join(cur_path,'Desktop',file)
-
-#client.download_file(Bucket=bucket, Key=file, Filename=filename)
-#client = boto3.resource('s3',
- #                 aws_access_key_id="AKIA25P26T62ZQL6HPPM",
- #                 aws_secret_access_key="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")
-
-#bucket = client.Bucket('code-airbnb-property-listings')
-
-
-
-#my_bucket = client.Bucket('code-airbnb-property-listings')
-#objects = my_bucket.objects.filter(Prefix='images/')
-#for obj in objects:
- #   path, filename = os.path.split(obj.key)
- #   my_bucket.download_file(obj.key, filename)
-
-
-
-
 def resize_images(): #gathers png images to convert to correct size and store in directory
     file_path = '/Users/vikasiniperemakumar/Desktop/AiCore/airbnb-property-listings/images' #path to images to convert
     total_folders_processed = 0 #initial number of image folders that have been converted
